560b3963359b3cdd8fbfc6327c8d881ea61a8623
thaumstrial <2738722282@qq.com>
2025-03-18 04:22:09 +0800

    feat: main menu choose

--- src/ui.rs
+++ src/ui.rs
@@ -1,4 +1,6 @@
 use bevy::color::palettes::basic::{GRAY, WHITE};
+use bevy::input::common_conditions::{input_pressed};
+use bevy::input::keyboard::KeyboardInput;
 use bevy::prelude::*;
 use crate::{AppState, AsciiFont, GameState, PlayerBombsText, PlayerGrazeText, PlayerLivesText, PlayerPointsText, PlayerPowersText, WindowSize};
 
@@ -6,6 +8,7 @@ use crate::{AppState, AsciiFont, GameState, PlayerBombsText, PlayerGrazeText, Pl
 #[source(AppState = AppState::MainMenu)]
 enum MainMenuState {
     #[default]
+    Choosing,
     Start,
     ExtraStart,
     PracticeStart,
@@ -15,30 +18,23 @@ enum MainMenuState {
     Option,
     Quit,
 }
+#[derive(Resource)]
+pub struct SelectedMenuEntry {
+    pub selected: MainMenuState,
+    pub repeat_timer: Timer,
+}
 #[derive(Component)]
 struct MainMenuEntry(MainMenuState);
-
-#[derive(Component)]
-struct StartText;
-#[derive(Component)]
-struct ExtraStartText;
-#[derive(Component)]
-struct PracticeStartText;
-#[derive(Component)]
-struct ReplayText;
-#[derive(Component)]
-struct ScoreText;
-#[derive(Component)]
-struct MusicRoomText;
-#[derive(Component)]
-struct OptionText;
-#[derive(Component)]
-struct QuitText;
 
 fn setup_main_menu(
     mut commands: Commands,
     font: Res<AsciiFont>,
 ) {
+    commands.insert_resource(SelectedMenuEntry {
+        selected: MainMenuState::Start,
+        repeat_timer: Timer::from_seconds(0.15, TimerMode::Repeating),
+    });
+
     let font_size = 40.0;
     let text_font = TextFont {
         font: font.0.clone(),
@@ -48,37 +44,50 @@ fn setup_main_menu(
 
     commands
         .spawn(Node {
-            flex_direction: FlexDirection::Column,
+            justify_content: JustifyContent::FlexEnd,
+            align_items: AlignItems::FlexEnd,
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
             ..default()
         })
         .with_children(|parent| {
-            let menu_items = vec![
-                (MainMenuState::Start, "Start"),
-                (MainMenuState::ExtraStart, "Extra Start"),
-                (MainMenuState::PracticeStart, "Practice Start"),
-                (MainMenuState::Replay, "Replay"),
-                (MainMenuState::Score, "Score"),
-                (MainMenuState::MusicRoom, "Music Room"),
-                (MainMenuState::Option, "Option"),
-                (MainMenuState::Quit, "Quit"),
-            ];
+           parent.spawn(Node {
+               flex_direction: FlexDirection::Column,
+               margin: UiRect {
+                   right: Val::Px(200.0),
+                   ..default()
+               },
+               ..default()
+           })
+               .with_children(|parent| {
+                   let menu_items = vec![
+                       (MainMenuState::Start, "> Start"),
+                       (MainMenuState::ExtraStart, "  Extra Start"),
+                       (MainMenuState::PracticeStart, "  Practice Start"),
+                       (MainMenuState::Replay, "  Replay"),
+                       (MainMenuState::Score, "  Score"),
+                       (MainMenuState::MusicRoom, "  Music Room"),
+                       (MainMenuState::Option, "  Option"),
+                       (MainMenuState::Quit, "  Quit"),
+                   ];
 
-            for (i, (state, label)) in menu_items.iter().enumerate() {
-                parent.spawn((
-                    Node {
-                        left: Val::Px((menu_items.len() - i) as f32 * font_size),
-                        ..default()
-                    }
-                )).with_children(|parent| {
-                    parent.spawn((
-                        MainMenuEntry(*state),
-                        Text::new(*label),
-                        text_font.clone(),
-                        TextLayout::new_with_justify(JustifyText::Left),
-                        TextColor(Color::Srgba(WHITE)),
-                    ));
-                });
-            }
+                   for (i, (state, label)) in menu_items.iter().enumerate() {
+                       parent.spawn((
+                           Node {
+                               left: Val::Px((menu_items.len() - i) as f32 * font_size * 0.5),
+                               ..default()
+                           }
+                       )).with_children(|parent| {
+                           parent.spawn((
+                               MainMenuEntry(*state),
+                               Text::new(*label),
+                               text_font.clone(),
+                               TextLayout::new_with_justify(JustifyText::Left),
+                               TextColor(Color::Srgba(WHITE)),
+                           ));
+                       });
+                   }
+               });
         });
 }
 
@@ -193,12 +202,74 @@ fn setup_in_game(
     ));
 }
 
+fn main_menu_selection(
+    time: Res<Time>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut selected: ResMut<SelectedMenuEntry>,
+    mut texts: Query<(&MainMenuEntry, &mut Text)>,
+) {
+    use MainMenuState::*;
+
+    let order = [
+        Start,
+        ExtraStart,
+        PracticeStart,
+        Replay,
+        Score,
+        MusicRoom,
+        Option,
+        Quit,
+    ];
+
+    let mut direction = 0;
+
+    if keyboard_input.just_pressed(KeyCode::ArrowUp) {
+        direction = -1;
+        selected.repeat_timer.reset();
+    } else if keyboard_input.just_pressed(KeyCode::ArrowDown) {
+        direction = 1;
+        selected.repeat_timer.reset();
+    } else if keyboard_input.pressed(KeyCode::ArrowUp) {
+        selected.repeat_timer.tick(time.delta());
+        if selected.repeat_timer.finished() {
+            direction = -1;
+        }
+    } else if keyboard_input.pressed(KeyCode::ArrowDown) {
+        selected.repeat_timer.tick(time.delta());
+        if selected.repeat_timer.finished() {
+            direction = 1;
+        }
+    } else {
+        selected.repeat_timer.reset();
+    }
+
+    if direction == 0 {
+        return;
+    }
+
+    let current = selected.selected;
+    let current_index = order.iter().position(|s| *s == current).unwrap_or(0);
+    let new_index = (current_index as isize + direction + order.len() as isize) % order.len() as isize;
+    let new_state = order[new_index as usize];
+    selected.selected = new_state;
+
+    for (entry, mut text) in texts.iter_mut() {
+        let label = text.0.trim_start_matches(['>', ' ']);
+        if entry.0 == new_state {
+            text.0 = format!("> {}", label);
+        } else {
+            text.0 = format!("  {}", label);
+        }
+    }
+}
+
 pub struct GameUiPlugin;
 impl Plugin for GameUiPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_sub_state::<MainMenuState>()
             .add_systems(OnEnter(AppState::MainMenu), setup_main_menu)
-            .add_systems(OnEnter(AppState::InGame), setup_in_game);
+            .add_systems(OnEnter(AppState::InGame), setup_in_game)
+            .add_systems(Update, main_menu_selection.run_if(in_state(AppState::MainMenu)));
     }
 }
